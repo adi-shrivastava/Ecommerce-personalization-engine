b2890f9e4fe627a71bf572a5d91e2006c4668ab7
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-23 00:19:33 +0530

    Added event logic and seeded in event table

--- backend/seed/seedevents.py
+++ backend/seed/seedevents.py
@@ -3,7 +3,13 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import random
 from db import conn,cursor
+userid=cursor.execute("Select userid from users")
+productid=cursor.execute("Select productid from products")
 eventtype=["view","addtocart","purchased"]
 for _ in range(10000):
     event=random.choice(eventtype)
+    cursor.execute("INSERT INTO events (userid,productid,eventtype) values(%s,%s,%s)",random.choice(userid),
+    random.choice(productid),event)
+cursor.close()
+conn.close()
     
