b14d2c741ec71ede1fd2c262d2dc33995e7ac53c
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-24 11:28:46 +0530

    Installed scikit-learn into directory as well as analyzed matrix.sum(axis=0) for max user and axis=1 for max product engagement extraction

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,3 +1,4 @@
+from sklearn.metrics.pairwise import code_similiarity
 import pandas as pd
 from db import conn,cursor
 query="""select userid,productid,
@@ -12,12 +13,15 @@ sum(case
  group by userid,productid;"""
 
 df=pd.read_sql(query,conn)
-print(df.head())
+# print(df.head())
 matrix=df.pivot_table(
     index="userid",
     columns="productid",
     values="score",
     fill_value=0
 )
-print(matrix.shape)
-print(matrix.head())
+# print(matrix.head())
+# print(matrix.sum(axis=0).sort_values(ascending=False))
+# print(matrix.sum(axis=1).sort_values(ascending=False))
+# print(matrix.loc[68].sort_values(ascending=False))
+# print(matrix.stack().sort_values(ascending=False).head(10))
