03910caaa49a05cc88759960457b881e92bdf6b9
Adi Shrivastava <shrivadi0405@gmail.com>
2026-07-02 22:30:50 +0530

    Added FastAPI server setup in backend/server.py

--- backend/server.py
+++ backend/server.py
@@ -1,2 +1,8 @@
-from fastapi import FASTAPI
-app=FASTAPI()
+from fastapi import FastAPI
+app=FastAPI()
+@app.get("/")
+def home():
+    return{"message": "Hello World"}
+@app.get("/recommend")
+def recommend():
+    return{"message": "This is the recommendation endpoint"}
