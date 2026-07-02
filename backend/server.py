fccb39be5ae56bcabd5ea55cde866a4e39c479c0
Adi Shrivastava <shrivadi0405@gmail.com>
2026-07-03 00:26:46 +0530

    Fixed response of recommend function

--- backend/server.py
+++ backend/server.py
@@ -9,8 +9,18 @@ def home():
 @app.get("/recommend/{id}")
 def get_recommendations(id:int):
     recc=recommend(id)
+    prodid=tuple(recc.keys())
+    cursor.execute("""select productid,category,price from products where productid in %s""",(prodid,))
+    rows=cursor.fetchall()
     result=[]
-    for product,score in recc.items():
-        query=("""select productid,category,price from products where productid=%s""")
-        result.append((product,score,pd.read_sql(query,conn,params=(product,))))
-    return{"message": "This is the recommendation endpoint","recommendations ":result}
+    for productid,category,price in rows:
+        result.append(("Product ID : ",productid,
+        "Category : ",category,
+        "Price : ",price,
+        "Recommendation Score : ",recc[productid]))
+    return {"message":"That's recommendation endpoint", "recommendations": result}
+    # result=[]
+    # for product,score in recc.items():
+    #     query=("""select productid,category,price from products where productid=%s""")
+    #     result.append((product,score,pd.read_sql(query,conn,params=(product,))))
+    # return{"message": "This is the recommendation endpoint","recommendations ":result}
